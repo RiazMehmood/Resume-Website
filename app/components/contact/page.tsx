dc4f007fc2e0e072b4cc896878f6e072df2d09f3
RiazMehmood <engr.riaz2010@gmail.com>
2023-03-22 16:22:08 +0500

    contact page added
    contact page added public folder updated

--- app/components/contact/page.tsx
+++ app/components/contact/page.tsx
@@ -1,6 +1,6 @@
 export default function Contact() {
-    return (<div>
-      Contact
+    return (<div className="flex align-middle justify-center">
+            <div className="text-3xl font-semibold my-10 md:col-span-1">Contact Me</div>
     </div>
     )
   }
