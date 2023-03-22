c7595381f3a1387ec2aa3f96ec826cd3e6294a99
RiazMehmood <engr.riaz2010@gmail.com>
2023-03-22 17:20:01 +0500

    updated project section

--- app/components/contact/page.tsx
+++ app/components/contact/page.tsx
@@ -1,6 +1,9 @@
 export default function Contact() {
-    return (<div className="flex align-middle justify-center">
-            <div className="text-3xl font-semibold my-10 md:col-span-1">Contact Me</div>
+  return (
+    <div className="container flex align-middle justify-center">
+      <div className="text-3xl font-semibold my-10 md:col-span-1">
+        Contact Me
+      </div>
     </div>
-    )
-  }
+  );
+}
--- app/components/projects/page.tsx
+++ app/components/projects/page.tsx
@@ -1,86 +1,94 @@
-import { BsLinkedin, BsTwitter, BsGithub } from "react-icons/bs";
 import Link from "next/link";
+import { BsFillArrowUpRightCircleFill, BsGithub } from "react-icons/bs";
 
 export default function Projects() {
   return (
-    <div className="min-h-[70vh] mx-10">
-      <p className="mt-18 text-4xl font-sans border-b-2">Projects</p>
-      <div className="bg-gray-100 -my-9">
-        <div className="grid grid-cols-[10rem,20rem,1fr,2rem] my-10 ">
-        <div className="text-4xl underline font-semibold"></div>
-          
-          <div>
-            <h1 className="text-2xl font-bold">Website Templates</h1>
+    <div className="min-h-screen mx-10 py-12">
+      <h1 className="text-4xl font-sans border-b-2 mb-8">Projects</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="bg-gray-100 p-8 rounded-lg">
+          <h2 className="text-2xl font-bold mb-4">Website Templates</h2>
+
+          <div className="flex items-center mb-4">
+            <BsGithub className="text-2xl mr-2" />
+            <Link
+              legacyBehavior
+              href="https://github.com/RiazMehmood/Resume-Website"
+            >
+              <a className="text-lg font-semibold hover:underline">
+                Resume
+              </a>
+            </Link>
+            <div className="flex items-center ml-4">
+              <Link legacyBehavior href="https://panaversedao.vercel.app/">
+                <a className="text-lg font-semibold hover:underline">
+                  www.myresume.com
+                </a>
+              </Link>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <BsGithub className="text-2xl mr-2" />
+            <Link
+              legacyBehavior
+              href="https://github.com/RiazMehmood/piaic-website"
+            >
+              <a className="text-lg font-semibold hover:underline">
+                PIAIC
+              </a>
+            </Link>
+            
+            <div className="flex items-center ml-4">
+              <Link legacyBehavior href="https://panaversedao.vercel.app/">
+                <a className="text-lg font-semibold hover:underline">
+                  www.panaversedao.com
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
-        <div className="grid grid-cols-[10rem,14rem,1fr,2rem] ">
-          <div></div>
-          <div className="text-xl font-semibold">
-            <h1>Resume Website</h1>
+
+        <div className="bg-gray-100 p-8 rounded-lg">
+          <h2 className="text-2xl font-bold mb-4">TypeScript CLI Projects</h2>
+
+          <div className="flex items-center mb-4">
+            <BsGithub className="text-2xl mr-2" />
+            <Link
+              legacyBehavior
+              href="https://github.com/RiazMehmood/Resume-Website"
+            >
+              <a className="text-lg font-semibold hover:underline">
+                Resume Website
+              </a>
+            </Link>
           </div>
-          <div className="text-2xl font-semibold">
-            <h2><Link href={'https://github.com/RiazMehmood/Resume-Website'}><BsGithub/></Link></h2>
+
+          <div className="flex items-center mb-4">
+            <BsGithub className="text-2xl mr-2" />
+            <Link
+              legacyBehavior
+              href="https://github.com/RiazMehmood/piaic-website"
+            >
+              <a className="text-lg font-semibold hover:underline">
+                PIAIC Website
+              </a>
+            </Link>
           </div>
-          <div></div>
+
+          <div className="flex items-center">
+            <BsGithub className="text-2xl mr-2" />
+            <Link
+              legacyBehavior
+              href="https://github.com/RiazMehmood/panaverse-website"
+            >
+              <a className="text-lg font-semibold hover:underline">
+                Panaverse Website
+              </a>
+            </Link>
+          </div>
         </div>
-        <div className="grid grid-cols-[10rem,14rem,1fr,2rem] ">
-          <div></div>
-          <div className="text-xl font-semibold">
-            <h1>PIAIC Website</h1>
-          </div>
-          <div className="text-2xl font-semibold">
-            <h2><Link href={'https://github.com/RiazMehmood/Resume-Website'}><BsGithub/></Link></h2>
-          </div>
-          <div></div>
-        </div>
-        <div className="grid grid-cols-[10rem,14rem,1fr,2rem] ">
-          <div></div>
-          <div className="text-xl font-semibold">
-            <h1>Panaverse Website</h1>
-          </div>
-          <div className="text-2xl font-semibold">
-            <h2><Link href={'https://github.com/RiazMehmood/Resume-Website'}><BsGithub/></Link></h2>
-          </div>
-          <div></div>
-        </div>
-        <div className="grid grid-cols-[10rem,20rem,1fr,2rem] my-10 ">
-        <div className="text-4xl underline font-semibold"></div>
-          
-          <div>
-            <h1 className="text-2xl font-bold">TypeScript CLI Projects</h1>
-          </div>
-        </div>
-        <div className="grid grid-cols-[10rem,14rem,1fr,2rem] ">
-          <div></div>
-          <div className="text-xl font-semibold">
-            <h1>Resume Website</h1>
-          </div>
-          <div className="text-2xl font-semibold">
-            <h2><Link href={'https://github.com/RiazMehmood/Resume-Website'}><BsGithub/></Link></h2>
-          </div>
-          <div></div>
-        </div>
-        <div className="grid grid-cols-[10rem,14rem,1fr,2rem] ">
-          <div></div>
-          <div className="text-xl font-semibold">
-            <h1>PIAIC Website</h1>
-          </div>
-          <div className="text-2xl font-semibold">
-            <h2><Link href={'https://github.com/RiazMehmood/Resume-Website'}><BsGithub/></Link></h2>
-          </div>
-          <div></div>
-        </div>
-        <div className="grid grid-cols-[10rem,14rem,1fr,2rem] ">
-          <div></div>
-          <div className="text-xl font-semibold">
-            <h1>Panaverse Website</h1>
-          </div>
-          <div className="text-2xl font-semibold">
-            <h2><Link href={'https://github.com/RiazMehmood/Resume-Website'}><BsGithub/></Link></h2>
-          </div>
-          <div></div>
-        </div>         
-        
       </div>
     </div>
   );
